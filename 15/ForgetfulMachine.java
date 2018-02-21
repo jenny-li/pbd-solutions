fb829d70055bf91bdccf7a0ce84db9b7da73b82d
jenny-li <yongjie.li18@ycdsbk12.ca>
2018-02-21 09:12:06 -0500

    Add files via upload

---
+++ 15/ForgetfulMachine.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ForgetfulMachine {
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.print( "Give me a word! " );
+		keyboard.next();
+
+		System.out.print( "Give me a second word! " );
+		keyboard.next();
+
+		System.out.print( "Great, now your favourite number? " );
+		keyboard.nextInt();
+
+		System.out.println("And your second-favourite number… ");
+        keyboard.nextInt();
+
+        System.out.println("Whew! Wasn't that fun?");
+    }
+
+}
+
